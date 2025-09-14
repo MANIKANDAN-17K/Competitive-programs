d7f3636c0a891e7172de6c8a439dbcd16ee50929
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2025-09-14 10:39:18 +0530

    Create FindFreqVowAndConson.java

---
+++ FindFreqVowAndConson.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxFreqSum(String s) {
+        int[] vowFreq = new int[26];
+        int[] conFreq = new int[26];
+        for(char ch : s.toCharArray()){
+            if("aeiou".contains(Character.toString(ch))){
+                vowCounter(ch,vowFreq);
+            }else{
+                conCounter(ch,conFreq);
+            }
+        }
+        int vowMax = Arrays.stream(vowFreq).max().getAsInt();
+        int conMax = Arrays.stream(conFreq).max().getAsInt();;
+    return vowMax + conMax;
+    }
+    private void vowCounter(char ch,int[] vowFreq){
+        vowFreq[ch - 'a']++;
+    }
+    private void conCounter(char ch,int[] conFreq){
+        conFreq[ch - 'a']++;
+    }
+}
