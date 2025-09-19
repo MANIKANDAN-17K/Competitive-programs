072c1a2c51f2048f2041715156c09d741b2809f0
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2025-09-19 17:49:21 +0530

    Create spreadSheet.java

---
+++ spreadSheet.java
@@ -0,0 +1,28 @@
+class Spreadsheet {
+    private HashMap<String,Integer> mpp;
+    public Spreadsheet(int rows) {
+        mpp = new HashMap<>();
+    }
+    
+    public void setCell(String cell, int value) {
+        mpp.put(cell,value);
+    }
+    
+    public void resetCell(String cell) {
+        mpp.put(cell,0);
+    }
+    
+    public int getValue(String formula) {
+        formula = formula.substring(1);
+        for(int i = 0;i<formula.length();i++){
+            if(formula.charAt(i) == '+'){
+                String s1 = formula.substring(0,i);
+                String s2 = formula.substring(i+1);
+                int left = Character.isUpperCase(s1.charAt(0)) ? mpp.getOrDefault(s1,0) : Integer.parseInt(s1);
+                int right = Character.isUpperCase(s2.charAt(0)) ? mpp.getOrDefault(s2,0) : Integer.parseInt(s2);
+                return left + right;
+            }
+        }
+        return 0;
+    }
+}
