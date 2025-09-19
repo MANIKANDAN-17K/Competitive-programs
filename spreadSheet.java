246415639c2fd09b64df64f87e0674758e6ebf55
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2025-09-19 17:52:24 +0530

    Update spreadSheet.java

--- spreadSheet.java
+++ spreadSheet.java
@@ -11,7 +11,7 @@ class Spreadsheet {
     public void resetCell(String cell) {
         mpp.put(cell,0);
     }
-    
+     
     public int getValue(String formula) {
         formula = formula.substring(1);
         for(int i = 0;i<formula.length();i++){
