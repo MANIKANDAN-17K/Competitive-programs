95eaa319902e726ea57d21c35207830458fe15ad
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2025-09-19 17:49:46 +0530

    Update makeTheIntegerZero.java

--- makeTheIntegerZero.java
+++ makeTheIntegerZero.java
@@ -1,7 +1,7 @@
 class Solution {
     public int makeTheIntegerZero(int num1, int num2) {
 
-       for(long k = 1; ;k++){
+       for(long k = 1; ;k++){ 
             long target = num1 - k * num2;
             if(target < 0) break;
             if(Long.bitCount(target) <= k && k<=target) return (int) k;
