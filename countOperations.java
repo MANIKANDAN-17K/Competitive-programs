fa504281b58cfb0bc9adf1b796f885aa37eee4a6
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2025-11-09 07:16:23 +0530

    2169. Count Operations to Obtain Zero
    Refactor countOperations method to handle base cases and optimize recursion.

---
+++ countOperations.java
@@ -0,0 +1,30 @@
+// TLE 
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int cnt = 0;
+        while(num1 != 0 || num2 != 0){
+            if(num1 < num2){
+                num2 -= num1;
+                cnt++;
+            }else if( num2 < num1){
+                num1 -= num2;
+                cnt++;
+            }else{
+                cnt++;
+                break;
+            }
+        }
+        return cnt;
+        
+    }
+}
+
+----------------------------------------------------------
+  class Solution {
+    public int countOperations(int num1, int num2) {
+        if(num1 == 0 || num2 == 0) return 0;
+        if(num1 < num2) return countOperations(num2,num1);
+        return (num1 / num2) + countOperations (num1 % num2, num2); 
+    }
+}
+-------------------------------------------------------------------
