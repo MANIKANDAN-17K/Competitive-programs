7db25a392038f34f271759e5cec72b9627276e7f
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2025-12-08 22:59:27 +0530

    1925. Count Square Sum Triples

---
+++ countTriples.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countTriples(int n) {
+        int res = 0;
+        for(int i = 1;i<= n;++i){
+            for(int j = 1;j<=n;++j){
+                int c = (int) Math.sqrt(i*i+j*j+0.1);
+                if(c <= n && c*c == i*i+j*j){
+                    ++res;
+                }
+            }
+        }
+        return res;
+    }
+}
