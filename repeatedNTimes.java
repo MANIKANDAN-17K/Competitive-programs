d978069278d6bf5367002bbb56473c5e43163951
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2026-01-02 20:22:22 +0530

    961. N-Repeated Element in Size 2N Array

---
+++ repeatedNTimes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        int n = nums.length;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<n;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue() * 2 ==  n){
+                return entry.getKey();
+            }
+        }
+        return 0;
+    }
+}
